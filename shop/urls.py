08d259325daa014a671da0a89cdd22dc7f4352f6
hoolywant <sir.saltickow@yandex.ru>
2023-08-26 14:08:06 +0400

    main work for controllers

--- shop/urls.py
+++ shop/urls.py
@@ -1,14 +1,20 @@
-from . import views
+from .apps import ShopConfig
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from shop.views import home, contacts
-from .apps import ShopConfig
+from shop.views import (contacts, ProductView, ProductListView, BlogListView,
+                        BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView)
+
 
 app_name = ShopConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', contacts, name='contacts'),
-    path('<int:pk>', views.NewDetailView.as_view(), name='product_info'),
+    path('<int:pk>', ProductView.as_view(), name='product_info'),
+    path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_view'),
+    path('edit/<int:pk>', BlogUpdateView.as_view(), name='blog_edit'),
+    path('delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,79 @@
+import datetime
+
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import (DetailView, ListView,
+                                  CreateView, UpdateView, DeleteView)
+from pytils.translit import slugify
 
-from shop.models import Product
+from shop.models import Product, Blog
 
 
-def home(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(request, 'shop/home.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
 def contacts(request):
     return render(request, 'shop/contacts.html')
 
 
-class NewDetailView(DetailView):
+class ProductView(DetailView):
     model = Product
-    template_name = 'shop/product.html'
-    context_object_name = 'product'
+
+
+class BlogListView(ListView):
+    model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(activity_sign=True)
+        return queryset
+
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'preview')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+            return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('shop:blog_view', args=[self.kwargs.get('pk')])
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'content', 'preview', 'activity_sign')
+    success_url = reverse_lazy('shop:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+            return super().form_valid(form)
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('shop:blog_list')
+
+
+
 
 
