a25c1716b458ac36c2c735170ee2f6ad48f4cf87
hoolywant <sir.saltickow@yandex.ru>
2023-08-26 14:05:54 +0400

    bodel for blog is ready

--- shop/models.py
+++ shop/models.py
@@ -21,3 +21,22 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='заголовок')
+    slug = models.CharField(max_length=100, verbose_name='slug', **NULLABLE)
+    content = models.TextField(verbose_name='контент')
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    activity_sign = models.BooleanField(verbose_name='активна')
+    view_count = models.IntegerField(default=0, verbose_name='количество просмотров')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
+
+
